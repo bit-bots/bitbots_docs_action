54f687ab32b2942aa2464cf9aa1f0a679d4411c3
Timon Engelke <timon.engelke@online.de>
2023-04-05 14:31:36 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd /github/workspace
 for d in $@; do
+    echo $SSHPASS | wc -c
     pushd $d
     sphinx-build docs docs/_out -b html
     lftp -c "open sftp://docs:$SSHPASS@doku.bit-bots.de; mirror -Re docs/_out package/$d"
